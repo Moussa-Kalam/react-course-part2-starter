39c0a8ea25d9c35a9e5c80a252b6f90146d7c72f
Moussa-Kalam <m.amzat@alustudent.com>
2024-03-24 22:57:47 +0200

    feat: get information about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,5 +1,12 @@
+import { useLocation, useParams, useSearchParams } from 'react-router-dom';
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation()
+  // console.log(searchParams.get('name'))
+
+  return <p>User {id}</p>;
 };
 
 export default UserDetailPage;
